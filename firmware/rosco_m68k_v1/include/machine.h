66da21028848923fe9387f7e96e7fa9c792f81ea
Ross Bamford <roscopeco@gmail.com>
2019-06-19 22:44:03 +0100

    Surface new routines from ASM

--- firmware/rosco_m68k_v1/include/machine.h
+++ firmware/rosco_m68k_v1/include/machine.h
@@ -19,6 +19,8 @@
  * Issues a STOP instruction, keeping the CPU in supervisor mode
  * and setting interrupt priority to 3.
  *
+ * Does not stop the system tick.
+ *
  * Requires the CPU to be in supervisor mode.
  */
 noreturn void IDLE();
@@ -26,8 +28,8 @@ noreturn void IDLE();
 /*
  * Halt the processor.
  *
- * Issues a STOP instruction, keeping the CPU in supervisor mode
- * and masking all interrupts (priority to 7).
+ * Issues a STOP instruction, keeping the CPU in supervisor mode,
+ * masking all interrupts (priority to 7) and disabling the MFP interrupts.
  *
  * The only way to come back from this is via the wetware!
  *
@@ -36,9 +38,30 @@ noreturn void IDLE();
 noreturn void HALT();
 
 /*
+ * Start the system tick (MFP Timer C).
+ *
+ * Note that this cannot be done until we've finished using polled
+ * (synchronous) IO on the UART (i.e. the EARLY_PRINT routines).
+ * Otherwise, timing issues will cause garbage on the serial port. 
+ */
+void START_HEART();
+
+/*
+ * Stop the system tick (MFP Timer C)
+ */
+void STOP_HEART();
+
+/*
  * Set m68k interrupt priority level (0-7).
  *
  * Requires the CPU to be in supervisor mode.
  */
 void SET_INTR(uint8_t priority);
+
+/*
+ * Early print null-terminated string.
+ *
+ * Don't use this after START_HEART has been called. 
+ */
+void EARLY_PRINT_C(char *str);
 
