e5df5fda0d26b331257f68f78f4554e546108f19
Ross Bamford <roscopeco@gmail.com>
2020-03-08 22:21:43 +0000

    Fix MFP registers for #7 workaround

--- firmware/rosco_m68k_v1/include/machine.h
+++ firmware/rosco_m68k_v1/include/machine.h
@@ -19,29 +19,29 @@
 
 // DEFINEs for MFP registers
 #define MFP_GPDR      0xf80001
-#define MFP_AER       0xf80003
-#define MFP_DDR       0xf80005
-#define MFP_IERA      0xf80007
+#define MFP_AER       0xf80021
+#define MFP_DDR       0xf80011
+#define MFP_IERA      0xf80031
 #define MFP_IERB      0xf80009
-#define MFP_IPRA      0xf8000B
-#define MFP_IPRB      0xf8000D
-#define MFP_ISRA      0xf8000F
-#define MFP_ISRB      0xf80011
-#define MFP_IMRA      0xf80013
+#define MFP_IPRA      0xf80029
+#define MFP_IPRB      0xf80019
+#define MFP_ISRA      0xf80039
+#define MFP_ISRB      0xf80005
+#define MFP_IMRA      0xf80025
 #define MFP_IMRB      0xf80015
-#define MFP_VR        0xf80017
-#define MFP_TACR      0xf80019
-#define MFP_TBCR      0xf8001B
+#define MFP_VR        0xf80035
+#define MFP_TACR      0xf8000D
+#define MFP_TBCR      0xf8002D
 #define MFP_TCDCR     0xf8001D
-#define MFP_TADR      0xf8001F
-#define MFP_TBDR      0xf80021
+#define MFP_TADR      0xf8003D
+#define MFP_TBDR      0xf80003
 #define MFP_TCDR      0xf80023
-#define MFP_TDDR      0xf80025
-#define MFP_SCR       0xf80027
-#define MFP_UCR       0xf80029
+#define MFP_TDDR      0xf80013
+#define MFP_SCR       0xf80033
+#define MFP_UCR       0xf8000B
 #define MFP_RSR       0xf8002B
-#define MFP_TSR       0xf8002D
-#define MFP_UDR       0xf8002F
+#define MFP_TSR       0xf8001B
+#define MFP_UDR       0xf8003B
 
 /* 
  * Idle the processor.
