2acc916bdf8bdc0183f67c1ccd80c6c18adda95b
Ross Bamford <roscopeco@gmail.com>
2020-03-09 22:26:28 +0000

    Fix link script and ROM naming

--- firmware/serial-receive/burn.sh
+++ firmware/serial-receive/burn.sh
@@ -2,8 +2,8 @@
 set -e
 
 read -p "Place EVEN ROM in burner"
-minipro -p AT28C64B -s -w rom_even.bin
+minipro -p AT28C64B -s -w serial_receive_even.bin
 
 read -p "Place ODD ROM in burner"
-minipro -p AT28C64B -s -w rom_odd.bin
+minipro -p AT28C64B -s -w serial_receive_odd.bin
 
