801f546d42741e55470e81cdfd119676f506e5fb
Ross Bamford <roscopeco@gmail.com>
2020-03-13 21:07:57 +0000

    Conditional compile for #7

--- firmware/serial-receive/include/machine.h
+++ firmware/serial-receive/include/machine.h
@@ -17,7 +17,8 @@
 
 #include <stdnoreturn.h>
 
-// DEFINEs for MFP registers
+#ifdef REVISION_0
+// DEFINEs for MFP registers on Revision 0 board
 #define MFP_GPDR      0xf80001
 #define MFP_AER       0xf80021
 #define MFP_DDR       0xf80011
@@ -42,6 +43,33 @@
 #define MFP_RSR       0xf8002B
 #define MFP_TSR       0xf8001B
 #define MFP_UDR       0xf8003B
+#else
+// DEFINEs for MFP registers on "fixed" boards
+#define MFP_GPDR      0xf80001
+#define MFP_AER       0xf80003
+#define MFP_DDR       0xf80005
+#define MFP_IERA      0xf80007
+#define MFP_IERB      0xf80009
+#define MFP_IPRA      0xf8000B
+#define MFP_IPRB      0xf8000D
+#define MFP_ISRA      0xf8000F
+#define MFP_ISRB      0xf80011
+#define MFP_IMRA      0xf80013
+#define MFP_IMRB      0xf80015
+#define MFP_VR        0xf80017
+#define MFP_TACR      0xf80019
+#define MFP_TBCR      0xf8001B
+#define MFP_TCDCR     0xf8001D
+#define MFP_TADR      0xf8001F
+#define MFP_TBDR      0xf80021
+#define MFP_TCDR      0xf80023
+#define MFP_TDDR      0xf80025
+#define MFP_SCR       0xf80027
+#define MFP_UCR       0xf80029
+#define MFP_RSR       0xf8002B
+#define MFP_TSR       0xf8002D
+#define MFP_UDR       0xf8002F
+#endif
 
 /* 
  * Idle the processor.
