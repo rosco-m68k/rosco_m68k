f1fa7240f7fefa09a51c6503e027eeefe5bfc890
Ross Bamford <roscopeco@gmail.com>
2020-03-28 04:41:55 +0000

    Deprecated old firmware, to move to software

--- code/firmware/rosco_m68k_v1/burn.sh
+++ code/firmware/rosco_m68k_v1/burn.sh
@@ -2,8 +2,8 @@
 set -e
 
 read -p "Place EVEN ROM in burner"
-minipro -p AT28C64B -s -w rom_even.bin
+minipro -p AT28C64B -s -w serial_receive.rom_even.bin
 
 read -p "Place ODD ROM in burner"
-minipro -p AT28C64B -s -w rom_odd.bin
+minipro -p AT28C64B -s -w serial_receive.rom_odd.bin
 
