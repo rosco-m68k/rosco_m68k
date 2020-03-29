87886bca4436e7c75cdcdecf521fd45feb48d02d
Ross Bamford <roscopeco@gmail.com>
2020-03-29 19:12:31 +0100

    Remove broken zmodem

--- code/firmware/rosco_m68k_v1/zmodem/include.mk
+++
@@ -1,4 +0,0 @@
-CFLAGS := $(CFLAGS) -Imbzm/include -DZEMBEDDED
-OBJECTS := $(OBJECTS) mbzm/zheaders.o mbzm/znumbers.o mbzm/zserial.o 		\
-			mbzm/crcccitt.o mbzm/crc32.o zmodem/zmodem_support.o 
-			
