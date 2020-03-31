d5bc285930d0d7857442704e678723fe22d200fd
Ross Bamford <roscopeco@gmail.com>
2020-04-01 00:39:02 +0100

    Remove link ordering dependency in init.S

--- code/software/poc-kernel/kinit.c
+++ code/software/poc-kernel/kinit.c
@@ -14,12 +14,11 @@
  */
 
 #include <stdint.h>
-#include "machine.h"
 
 // Linker defines
 extern uint32_t _data_start, _data_end, _code_end, _bss_start, _bss_end;
 
-void kinit() {
+void __kinit() {
   // zero .bss
   for (uint32_t *dst = &_bss_start; dst < &_bss_end; *dst++ = 0);
 }
