daab2889635853897492e1c3d843777ec72b1ce2
Ross Bamford <roscopeco@gmail.com>
2020-04-01 19:28:12 +0100

    Add doc for restart

--- code/software/starter_c/kmain.c
+++ code/software/starter_c/kmain.c
@@ -7,9 +7,12 @@
 #include <stdnoreturn.h>
 #include <stdbool.h>
 
-extern void *PREMAIN;
-
 void kmain() {
   printf("Hello, world! 😃\r\n");
+
+  // When this function returns, the machine will reboot.
+  //
+  // In a real program, you probably wouldn't return from 
+  // this function!
 }
 
