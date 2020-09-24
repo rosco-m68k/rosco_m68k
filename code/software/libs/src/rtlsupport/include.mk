9d37578840bfd96f6cf2cedc9d6c3dc57765dc19
Ross Bamford <roscopeco@gmail.com>
2020-09-24 23:02:10 +0100

    Revert "Remove rtlsupport, replace with libgcc"
    This reverts commit f4f4e0bcb3065ccc4d11f055cce8b3613889ba51.

---
+++ code/software/libs/src/rtlsupport/include.mk
@@ -0,0 +1,16 @@
+LIB=rtlsupport
+LIBOBJECTS=$(DIR)/rtlsupport.o
+LIBINCLUDES=$(DIR)/include
+
+# ---===---
+DIR := $(shell dirname $(lastword $(MAKEFILE_LIST)))
+UPPERLIB := $(shell echo $(LIB) | tr '[:lower:]' '[:upper:]')
+BINARY := lib$(LIB).a
+CFLAGS  := $(CFLAGS) -I$(LIBINCLUDES) -DBUILD_ROSCOM68K_$(UPPERLIB)_LIB
+OBJECTS := $(OBJECTS) $(LIBOBJECTS)
+INCLUDES := $(INCLUDES) $(DIR)/include/*
+LIBS := $(LIBS) $(DIR)/$(BINARY)
+
+$(DIR)/$(BINARY): $(LIBOBJECTS)
+	$(AR)	$(ARFLAGS) rs $@ $^
+	$(RANLIB) $@
