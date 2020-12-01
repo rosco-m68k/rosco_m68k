5cbac0411c9a2dbb4b303e1a2eb91e4d44eddbb9
mattuna15 <matthew.pearce@sky.uk>
2020-12-01 14:18:27 +0000

    Add Game of Life

--- code/software/libs/src/cstdlib/stdlib.c
+++ code/software/libs/src/cstdlib/stdlib.c
@@ -1,4 +1,8 @@
 // basic stdlib functions
+#include <stdint.h>
+#include <stdlib.h>
+
+static volatile uint32_t * const upticks = (volatile uint32_t * const)0x40C;
 
 void exit(int status)
 {
@@ -16,4 +20,18 @@ void abort(void)
       "moveal 0xfc0004.l, %a0\n\t"
       "jmp %a0@\n\t"
   );
+}
+
+int rand(void) 
+{
+        long k;
+        long s = *upticks;
+        if (s == 0)
+          s = 0x12345987;
+        k = s / 127773;
+        s = 16807 * (s - k * 127773) - 2836 * k;
+        if (s < 0)
+          s += 2147483647;
+        //(*seed) = (unsigned int)s;
+        return (int)(s & RAND_MAX);
 }
