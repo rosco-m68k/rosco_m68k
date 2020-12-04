a936cbf7702c2a9210ab10a1aef304961fc15395
mattuna15 <matthew.pearce@sky.uk>
2020-12-04 12:49:20 +0000

    add more examples.

--- code/software/libm-test/kmain.c
+++ code/software/libm-test/kmain.c
@@ -12,7 +12,11 @@ void kmain() {
 
   float result = sin(35);
   double result2 = sqrt(4);
+  float result3 = cos(45);
+  float result4 = log(20);
   printf("sin(35) =  %.02f \r\n", result);
   printf("sqrt(4) =  %.02f \r\n", result2);
+  printf("cos(45)  =  %.02f \r\n", result3);
+  printf("log(20) =  %.02f \r\n", result4);
 }
 
