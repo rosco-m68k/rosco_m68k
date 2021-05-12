cb79e5110235a546d76e8dedbf49be70cbec076c
Ross Bamford <roscopeco@gmail.com>
2021-05-12 19:32:25 +0100

    First cut Xosera console

---
+++ code/firmware/rosco_m68k_development/videoXosera/include.mk
@@ -0,0 +1,2 @@
+EXTRA_CFLAGS := $(EXTRA_CFLAGS) -DXOSERA_CON -IvideoXosera/include
+OBJECTS := $(OBJECTS) videoXosera/xvidcon.o
---
+++ code/firmware/rosco_m68k_development/videoXosera/include/xosera.h
@@ -0,0 +1,37 @@
+/*
+ *------------------------------------------------------------
+ *                                  ___ ___ _
+ *  ___ ___ ___ ___ ___       _____|  _| . | |_
+ * |  _| . |_ -|  _| . |     |     | . | . | '_|
+ * |_| |___|___|___|___|_____|_|_|_|___|___|_,_|
+ *                     |_____|       firmware v2
+ * ------------------------------------------------------------
+ * Copyright (c)2020-2021 Ross Bamford and contributors
+ * See top-level LICENSE.md for licence information.
+ *
+ * Xosera console low-level primitives
+ * ------------------------------------------------------------
+ */
+
+#ifndef __ROSCO_M68K_XOSERA_CON_H
+#define __ROSCO_M68K_XOSERA_CON_H
+
+#include <stdbool.h>
+
+/**
+ * Returns true if a Xosera is installed, false otherwise.
+ */
+bool HAVE_XOSERA();
+
+/**
+ * Initialize the Xosera console.
+ */
+void XOSERA_CON_INIT();
+
+/**
+ * Install handlers for syscall PRINT/PRINTLN
+ */
+void XOSERA_CON_INSTALLHANDLERS();
+
+#endif  //  __ROSCO_M68K_XOSERA_CON_H
+
