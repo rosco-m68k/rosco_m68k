c16264f23a75eb9bf81896e8b743aaed824f6bf0
Xark <Xark@XarkLabs.com>
2021-10-06 14:53:32 -0700

    libm-test tweaks

--- code/software/libm-test/kmain.c
+++ code/software/libm-test/kmain.c
@@ -1,24 +1,31 @@
 /*
- * Copyright (c) 2020 Matthew Pearce (mattpearce@me.com)
+ * vim: set et ts=4 sw=4
+ *------------------------------------------------------------
+ *                                  ___ ___ _
+ *  ___ ___ ___ ___ ___       _____|  _| . | |_
+ * |  _| . |_ -|  _| . |     |     | . | . | '_|
+ * |_| |___|___|___|___|_____|_|_|_|___|___|_,_|
+ *                     |_____|
+ * ------------------------------------------------------------
+ * Copyright (c)2020 Ross Bamford
+ * See top-level LICENSE.md for licence information.
+ *
+ * rosco_m68k "kernel main" for bare-metal programs
+ * ------------------------------------------------------------
  */
 
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#include <basicio.h>
 #include <machine.h>
-#include <math.h>
-#include <printf.h>
 
-void kmain() {
+extern void main(int argc, char **argv);
 
-  float result = sin(35);
-  double result2 = sqrt(4);
-  float result3 = cos(45);
-  float result4 = log(20);
-  float result5 = exp(20);
-  printf("sin(35) =  %.02f \r\n", result);
-  printf("sqrt(4) =  %.02f \r\n", result2);
-  printf("cos(45)  =  %.02f \r\n", result3);
-  printf("log(20) =  %.02f \r\n", result4);
-  printf("exp(20) =  %.02f \r\n", result5);
+void kmain()
+{
+    main(0, NULL);
 }
-
