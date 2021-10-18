a03c255d683b0ca1a98e96abefb931eb81b23799
Ross Bamford <roscopeco@gmail.com>
2021-10-18 21:42:38 +0100

    Fix incorrect device

--- code/pld/r2/address_decoder/burn.sh
+++ code/pld/r2/address_decoder/burn.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-minipro -p ATF16V8BQL -w ic2_address_decoder.jed
+minipro -p ATF22V10C\(UES\) -w ic2_address_decoder.jed
 
--- code/pld/r2/duartsel/burn.sh
+++ code/pld/r2/duartsel/burn.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-minipro -p ATF16V8BQL -w ic3_duart_sel.jed
+minipro -p ATF22V10C\(UES\) -w ic3_duart_sel.jed
 
