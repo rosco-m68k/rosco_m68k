ef95fa9e0bbdac05c005edf42cadeddecf9e8a19
Xark <Xark@XarkLabs.com>
2022-07-31 13:50:35 -0700

    fix build issue with updated API

--- code/software/chardevs/kmain.c
+++ code/software/chardevs/kmain.c
@@ -6,19 +6,6 @@
 #include <machine.h>
 #include <printf.h>
 
-typedef struct {
-    uint32_t    data;
-    uint32_t    checkptr;
-    uint32_t    recvptr;
-    uint32_t    sendptr;
-    uint32_t    reserved0;
-    uint32_t    reserved1;
-    uint32_t    reserved2;
-    uint16_t    capabilities;
-    uint8_t     flags;
-    uint8_t     device_type;
-} __attribute__((packed)) CHAR_DEVICE;
-
 void kmain() {
     if (mcCheckDeviceSupport()) {
         printf("Character device support detected\n");
@@ -27,17 +14,20 @@ void kmain() {
         printf("Found %d device(s)\n", count);
 
         for (int i = 0; i < count; i++) {
-            CHAR_DEVICE *dev = mcGetDevice(i);
+            CharDevice dev;
+            if (mcGetDevice(i, &dev))   {
+                printf("type    : 0x%02x\n",  dev.device_type);
+                printf("data    : 0x%08lx\n", dev.data);
+                printf("checkptr: 0x%08lx\n", dev.checkptr);
+                printf("recvptr : 0x%08lx\n", dev.recvptr);
+                printf("sendptr : 0x%08lx\n", dev.sendptr);
 
-            printf("type    : 0x%02x\n",  dev->device_type);
-            printf("data    : 0x%08lx\n", dev->data);
-            printf("checkptr: 0x%08lx\n", dev->checkptr);
-            printf("recvptr : 0x%08lx\n", dev->recvptr);
-            printf("sendptr : 0x%08lx\n", dev->sendptr);
-
-            printf("Printing to device %d\n", i);
-            fctprintf(mcSendDevice, dev, "Hello, World\n"); 
-            printf("\n\n");
+                printf("Printing to device %d\n", i);
+                fctprintf(mcSendDevice, &dev, "Hello, World\n"); 
+                printf("\n\n");
+            } else {
+                printf("mcGetDevice for device %d failed.\n", i);
+            }
         }
 
         printf("Done\n");
