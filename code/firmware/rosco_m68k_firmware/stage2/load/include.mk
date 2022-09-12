3a8b8c5102fb326aed4d440bf0dfd06f22079e83
Thomas Jager <thomasj@thomasjager.ca>
2022-09-12 11:35:31 -0400

    Don't use custom build for `stage2/load/load.o` `load.o` was being built without the correct command-line options. Also modify `stage2/load/load.c` to not fail when errors are enabled.

--- code/firmware/rosco_m68k_firmware/stage2/load/include.mk
+++ code/firmware/rosco_m68k_firmware/stage2/load/include.mk
@@ -4,8 +4,4 @@ OBJECTS := $(OBJECTS) load/load.o																						\
 	load/fat_io_lib/fat_misc.o load/fat_io_lib/fat_string.o										\
 	load/fat_io_lib/fat_table.o load/fat_io_lib/fat_write.o
 
-EXTRA_CFLAGS := $(EXTRA_CFLAGS) -DFATFS_USE_CUSTOM_OPTS_FILE								\
-		-Iload/include -I../include
-		
-load/load.o: load/load.c
-	$(CC) $(EXTRA_CFLAGS) -c -o $@ $<
+EXTRA_CFLAGS := $(EXTRA_CFLAGS) -DFATFS_USE_CUSTOM_OPTS_FILE -Iload/include
