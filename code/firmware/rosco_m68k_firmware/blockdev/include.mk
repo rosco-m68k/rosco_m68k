7fa5df323f0941d54fd2842d56aba10300d15a3c
Thomas Jager <thomasj@thomasjager.ca>
2022-09-14 10:03:45 -0400

    Cleanup multiple `include.mk`

--- code/firmware/rosco_m68k_firmware/blockdev/include.mk
+++ code/firmware/rosco_m68k_firmware/blockdev/include.mk
@@ -1,9 +1,9 @@
+OBJECTS := $(OBJECTS) blockdev/bbspi.o blockdev/bbsd.o                  \
+						blockdev/syscalls_asm.o blockdev/ata.o
 ifeq ($(REVISION1X),true)
-OBJECTS := $(OBJECTS) blockdev/mfp_gpio.o blockdev/bbspi.o blockdev/bbsd.o                  \
-						blockdev/syscalls_asm.o blockdev/ata.o
+OBJECTS := $(OBJECTS) blockdev/mfp_gpio.o
 else
-OBJECTS := $(OBJECTS) blockdev/bbspi.o blockdev/bbsd.o blockdev/dua_spi_asm.o              \
-						blockdev/syscalls_asm.o blockdev/ata.o
+OBJECTS := $(OBJECTS) blockdev/dua_spi_asm.o
 endif
 EXTRA_CFLAGS := $(EXTRA_CFLAGS) -DROSCO_M68K_SDCARD -DSD_BLOCK_READ_ONLY -DBLOCKDEV_SUPPORT \
 								-DROSCO_M68K_ATA -Iblockdev/include
--- code/firmware/rosco_m68k_firmware/video9958/include.mk
+++ code/firmware/rosco_m68k_firmware/video9958/include.mk
@@ -1,2 +1,2 @@
-EXTRA_CFLAGS := $(EXTRA_CFLAGS) -DVIDEO9958_CON -Ivideo9958/include
 OBJECTS := $(OBJECTS) video9958/vdpcon.o video9958/font.o
+EXTRA_CFLAGS := $(EXTRA_CFLAGS) -DVIDEO9958_CON -Ivideo9958/include
