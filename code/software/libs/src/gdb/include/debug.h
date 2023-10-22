62d57b183c496746b7469f00f562b20d77e9d185
Ross Bamford <roscopeco@gmail.com>
2023-10-22 14:26:29 +0100

    A bit of doc

--- code/software/libs/src/gdb/include/debug.h
+++ code/software/libs/src/gdb/include/debug.h
@@ -16,7 +16,33 @@
 #ifndef _ROSCOM68K_GDB_H
 #define _ROSCOM68K_GDB_H
 
+/**
+ * Start debugging. Installs the debugging infrastructure into
+ * your process and takes over UART B for debugging purposes.
+ * 
+ * You'll probably want to execute a `breakpoint` immediately
+ * after this to pause the system until your debugger connects.
+ * 
+ * @return true if debugging can be supported on your system
+ * @return false if debugging cannot be supported on your system
+ */
 bool start_debugger(void);
+
+/**
+ * Clean-up the debugging infrastructure, and put traps and 
+ * IRQs back the way they were.
+ * 
+ * You don't _have_ to call this, but your system might not
+ * function properly until you hard reset if you don't.
+ */
+void cleanup_debugger(void);
+
+/**
+ * Execute a software breakpoint, and trap to the debugger.
+ * 
+ * If the debugger is not yet connected, this will pause until it
+ * is, which allows you to wait for connection. 
+ */
 void breakpoint(void);
 
 #endif//_ROSCOM68K_GDB_H
