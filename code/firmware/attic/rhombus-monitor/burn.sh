b96611ee45f41e66de8e1bc07c0582c049c102b6
Ross Bamford <roscopeco@gmail.com>
2023-10-28 17:41:39 +0100

    Delete firmware attic (#379)

--- code/firmware/attic/rhombus-monitor/burn.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/env bash
-set -e
-
-read -p "Place EVEN ROM in burner"
-minipro -p AT28C64B -s -w monitor_even.bin
-
-read -p "Place ODD ROM in burner"
-minipro -p AT28C64B -s -w monitor_odd.bin
-
