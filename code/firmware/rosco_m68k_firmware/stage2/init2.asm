a1a8d0e4bc8f81274984c89f9fc22a6d0439b974
Thomas Jager <thomasj@thomasjager.ca>
2024-03-21 10:08:44 -0400

    Change MFP use of SDB_SYSFLAGS, don't clear pins with system-use flag cleared

--- code/firmware/rosco_m68k_firmware/stage2/init2.asm
+++ code/firmware/rosco_m68k_firmware/stage2/init2.asm
@@ -24,10 +24,9 @@ START::
 
 red_led_off::
     ifd REVISION1X
-    move.b  MFP_GPDR,D0                 ; Get GPDR
-    or.b    #2,D0                       ; Turn off I1
-    and.b   D1,D0                       ; Mask with flags
-    move.b  D0,MFP_GPDR                 ; Set GPDR
+    move.b  SDB_SYSFLAGS,D0             ; Get sysflags (high byte)
+    and.b   #2,D0                       ; Turn off I1
+    or.b    D0,MFP_GPDR                 ; Update GPDR
     else
     move.l  SDB_UARTBASE,A0
     move.b  #$08,W_OPR_RESETCMD(A0)     ; Turn off red LED on r2.x boards
