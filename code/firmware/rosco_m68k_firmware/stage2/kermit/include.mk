155abc661e5e3ee6f71b8a7bdf52ab747dc037a9
Thomas Jager <thomasj@thomasjager.ca>
2024-05-13 21:20:06 -0400

    Fix bugs with Kermit define and HUGEROM

--- code/firmware/rosco_m68k_firmware/stage2/kermit/include.mk
+++ code/firmware/rosco_m68k_firmware/stage2/kermit/include.mk
@@ -1,7 +1,8 @@
 OBJECTS+=kermit/kermit.o kermit/kermit_support.o
+DEFINES+=-DKERMIT_LOADER
 INCLUDES+=-Ikermit/include
 
-KERMIT_DEFINES:=-DNODEBUG -DRECVONLY -DNO_CTRLC -DSTATIC=static -DKERMIT_LOADER
+KERMIT_DEFINES:=-DNODEBUG -DRECVONLY -DNO_CTRLC -DSTATIC=static
 KERMIT_EXTRA_CFLAGS=									\
 	-Wno-maybe-uninitialized -Wno-unused-variable		\
 	-Wno-unused-but-set-variable -Wno-stringop-overflow	\
