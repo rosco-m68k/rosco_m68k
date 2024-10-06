e52c783afc78a1b2c221468799b95516fade3cd9
Ross Bamford <roscopeco@gmail.com>
2024-10-06 12:27:05 +0100

    Add dir traversal to SD demo

--- code/software/newlib/libc_sd/main.c
+++ code/software/newlib/libc_sd/main.c
@@ -1,19 +1,53 @@
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <rosco_m68k/debug.h>
 
 #define BUF_SIZE    64
 
 static void existing_file_test();
 static void new_file_test();
+static void list_directory(const char *path);
 
 int main() {
     printf("## libc file test\n\n");
 
     existing_file_test();
     new_file_test();
-    
+
+    list_directory("/sd/");
+
+    FILE *f = fopen("/sd/gemsys", "r");
+    if (f) {
+        printf("fopen dir okay\n");
+        fclose(f);
+    } else {
+        printf("fopen dir not okay\n");
+    }
+
+        
+    struct stat statbuf;
+    if (stat("/sd/gemsys", &statbuf) == 0) {
+        printf("Stat ok\n");
+        if (S_ISDIR(statbuf.st_mode)) {
+            printf("Is a dir\n");
+        } else {
+            printf("Is not a dir\n");
+        }
+
+        /*
+        int subdir_count = count_entries_in_directory(full_path);
+        if (subdir_count >= 0) {
+            printf("  --> Subdirectory '%s' contains %d entries\n", entry->d_name, subdir_count);
+        }
+        */
+    } else {
+        printf("Stat not okay\n");
+    }
+
     printf("Tests are done!\n");
 
     breakpoint();
@@ -85,5 +119,25 @@ static void new_file_test() {
     do_read_test("/sd/test2.txt");
 
     printf("New file test done\n\n");
+}
+
+static void list_directory(const char *path) {
+    struct dirent *entry;
+    DIR *dp = opendir(path);
+
+    if (dp == NULL) {
+        perror("opendir");
+        return;
+    }
+
+    // Loop through directory entries
+    while ((entry = readdir(dp)) != NULL) {
+        // Ignore "." and ".." entries
+        if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
+            printf("%s [DIR: %d]\n", entry->d_name, entry->d_is_dir);
+        }
+    }
+
+    closedir(dp);
 }
 
